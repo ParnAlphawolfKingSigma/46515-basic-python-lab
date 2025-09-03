c6f6fa05c7f13f035e425d544dc76ed0ed27124b
ParnAlphawolfKingSigma <justgarl1c9@gmail.com>
2025-09-03 11:36:26 +0700

    loop
    ข้อ1 นับเลขจนถึง n
    ข้อ 2 นับถอยหลังตั้งแต่n
    ข้อ3 เป็นตารางสูตรคูณ
    ข้อ 4 บวกทุกจำนวนจนถึงn
    ข้อ 5 บวกแค่เลขคู่จนถึงn
    ข้อ 6 บวกแค่เลขคี่จนถึงn

---
+++ Loop-Python/02-count_down.py
@@ -0,0 +1,3 @@
+number = int(input())
+for n in range(number, 0, -1):
+    print(n)
---
+++ Loop-Python/06-sum_odd.py
@@ -0,0 +1,6 @@
+n = int(input())
+total = 0
+for i in range(1,1+n):
+    if i%2 != 0:
+        total += i
+print(total)
